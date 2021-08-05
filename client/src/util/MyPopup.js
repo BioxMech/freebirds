10533964d7118e6e2d1090eff8e1871719a2e184
Jason <livelivelivelive12345@gmail.com>
2021-08-05 17:33:59 +0800

    Update MyPopup.js

--- client/src/util/MyPopup.js
+++ client/src/util/MyPopup.js
@@ -1,8 +1,23 @@
-import { Popup } from 'semantic-ui-react';
 import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+import Typography from '@material-ui/core/Typography';
 
 function MyPopup({ content, children }) {
-  return <Popup inverted content={content} trigger={children} />
+
+  return (
+    <Tooltip title={
+      <Typography variant="body2">
+        { content }
+      </Typography>
+    } 
+      TransitionComponent={Zoom} 
+      TransitionProps={{ timeout: 300 }} 
+      arrow
+    >
+      { children }
+    </Tooltip>
+  )
 }
 
 export default MyPopup;
