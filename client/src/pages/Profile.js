55a6d1d2769f2c830ef32c738bd25cd770e36762
Jason <livelivelivelive12345@gmail.com>
2021-08-05 11:31:56 +0800

    Create Profile.js

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+// import React, { useContext, useState } from 'react';
+// import { AuthContext } from '../context/auth';
+
+function Profile () {
+  // const { user } = useContext(AuthContext);
+
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
